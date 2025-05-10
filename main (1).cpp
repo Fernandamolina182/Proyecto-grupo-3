2e58a51a5b625dbaacb6a9070956d16c0a126627
Fernandamolina182 <dmolina293@uan.edu.co>
2025-05-10 14:26:46 -0500

    Add files via upload
    Hola camilo buenas tardes, el profesor juan carlos nos dijo que como nuestro proyecto tenia mayor complejidad para nuestro proyecto el 50% eran 3 requerimientos , dijo que hicieramos enfasis  en el 3r requerimiento ya que los otros dependen de estructuras compuestas entre  uno y otro . gracias

--- main (1).cpp
+++ main (1).cpp
@@ -1,80 +1,132 @@
 #include <iostream>
 using namespace std;
-// primer requerimiento funcional
 
-struct silla {
+const int tam=50;
+struct Reserva {
     string clase;
     string ubicacion;
     string disponibilidad;
-  
+    string fecha;
+    int idPasajero;
+};
+struct DatosPasajero{
+    int cedulaPasajero;
+    string nombre;
 };
 
 int main() {
-      int a;
-      cout<<"Ingrese el numero de sillas a registrar:";
-      cin>>a;
-      int tam = a; // definir tamaño de la cantidad de sillas
-       int b,i;
-    cout<<"Ingrese el numero de reservas a registrar:";
-    cin>>b;
-    int tam1=b;
-    
-    silla listasillas[tam];
-
-    for (int i = 0; i < tam; i++) {
-        cout << "Ingrese los datos para la silla " << i <<endl;
-        cout << "Clase de la silla (Ejecutiva/Económica): ";
-        cin >> listasillas[i].clase;
-        cout << "Ubicación de la silla (Ventana/Centro/Pasillo): ";
-        cin >> listasillas[i].ubicacion;
-        cout << "Disponibilidad (si/no): ";
-        cin >> listasillas[i].disponibilidad;
-    }
-//Las posiciones inician desde 0
-    cout << "Información de las sillas registradas:"<<endl;
-    for (int i = 0; i < tam; i++) {
-        cout << "Silla " << i <<endl;
-        cout << "Clase: " << listasillas[i].clase << endl;
-        cout << "Ubicación: " << listasillas[i].ubicacion << endl;
-        cout << "Disponibilidad: " << listasillas[i].disponibilidad << endl;
+    Reserva listasillas[tam]; // Crear arreglo fijo de 50 sillas
+    DatosPasajero listaPasajeros[tam];
+    int cantidad;
+    cout << "Ingrese el número de sillas a registrar (máximo " << tam << "): ";
+    cin >> cantidad;
+    if (cantidad > tam) {
+        cout << "Número excede el límite de " << tam << ". Se ajustará al máximo permitido.\n";
+        cantidad = tam;
     }
 
+    int opcion, iddes, cedula, cantidaddepasajero;
+    string posicion, clase, nombre;
+    int sillasRegistradas = 0, reservasRegistradas = 0; // Para llevar control de datos ingresados
 
+    do {
+        cout << "\nMenú de opciones:\n";
+        cout << "1. Registrar sillas\n";
+        cout << "2. Mostrar información de sillas\n";
+        cout << "3. Registrar reservas\n";
+        cout << "4. Mostrar información de reservas\n";
+        cout << "5. Calcular el porcentaje de ocupación del avión\n";
+        cout << "6. Consultar el valor total de ventas por sillas ocupadas\n";
+        cout << "7. Consultar el promedio de venta por pasajero\n";
+        cout << "8. Salir del programa\n";
+        cout << "Ingrese su opción: ";
+        cin >> opcion;
 
-// segundo requerimiento funcional
-struct Reserva{
-    //atributos
-   int idReserva;
-    int idSillas;
-    int fecha;
-    int idPasajero;
-};
+        switch (opcion) {
+            case 1:
+                if (cantidad <= tam) {  
+                    for (int i = 0; i < cantidad; i++){
+                        cout << "Ingrese los datos para la silla " << i <<endl;
+                        cout << "Clase de la silla (Ejecutiva/Económica): ";
+                        cin >> listasillas[i].clase;
+                        cout << "Ubicación de la silla (Ventana/Centro/Pasillo): ";
+                        cin >> listasillas[i].ubicacion;
+                        cout << "Disponibilidad (si/no): ";
+                        cin >> listasillas[i].disponibilidad;
+                    }
+                } else {
+                    cout << "No se pueden registrar más sillas. Capacidad máxima alcanzada.\n";
+                }
+                break;
 
+            case 2:
+                if (cantidad <= tam) {  
+                    cout << "\nInformación de las sillas registradas:\n";
+                    for (int i = 0; i < cantidad; i++) {
+                        cout << "Silla " << i <<endl;
+                        cout << "Clase: " << listasillas[i].clase << endl;
+                        cout << "Ubicación: " << listasillas[i].ubicacion << endl;
+                        cout << "Disponibilidad: " << listasillas[i].disponibilidad << endl;
+                    }
+                } else {
+                    cout << "No hay sillas registradas aún.\n";
+                }
+                break;
 
-   
-    
+            case 3:
+                int sillaEscogida;
+                if (cantidad <= tam) {  
+                    cout << "Ingrese el número de la silla escogida: ";
+                    cin >> sillaEscogida;
+                    if (sillaEscogida < 1 || sillaEscogida > cantidad) {  
+                        cout << "Número de silla inválido.\n";
+                        break;
+                    }
+                    cout << "Ingrese la fecha de la reserva (dd/mm/aa): ";
+                    cin >> listasillas[sillaEscogida - 1].fecha;
+                    cout << "Ingrese el número del pasajero: ";
+                    cin >> listasillas[sillaEscogida - 1].idPasajero;
+                    reservasRegistradas++;
+                } else {
+                    cout << "No se pueden registrar más reservas. Capacidad máxima alcanzada.\n";
+                }
+                break;
 
-    Reserva listaReservas[tam1];
+            case 4:
+                if (cantidad <= tam) {  
+                    for (int i = 0; i < cantidad; i++) {
+                        cout << "\nIngrese los datos del pasajero " << i + 1 << ":\n";
+                        cout << "Número de cédula: ";
+                        cin >> listaPasajeros[i].cedulaPasajero;
+                        cout << "Nombre: ";
+                        cin >> listaPasajeros[i].nombre;
+                    }
+                } else {
+                    cout << "No hay reservas registradas aún.\n";
+                }
+                break;
 
-    {
-       for(int i=0; i<tam1; i++){
-        cout<<" Elije el número de la reserva:";
-        cin>>b;
-        cout<<"Ingrese el número de la silla escogida:"<<endl;
-        cin>>listaReservas[i].idSillas;
-        cout<<"Ingrese la fecha en que se hizo la reserva(dd/mm/aa):"<<endl;
-        cin>>listaReservas[i].fecha;
-        cout<<"Ingrese el número del pasajero:"<<endl;
-        cin>>listaReservas[i].idPasajero;
-        };
-    }
-    
-// las posiciones van desde cero hasta el tamaño de reserva.
-     cout<<" La informacíon de reserva registrada es:";
-      for(int i=0;i<tam1;i++){
-     cout<<" N° de reserva: "<<b<<" N° de silla:" <<listaReservas[i].idSillas<<" fecha: "
-     <<listaReservas[i].fecha<<" N° de pasajero: "<<listaReservas[i].idPasajero;
-    };
-         return 0;     
+            case 5:
+                cout << "Calcular el porcentaje de ocupación del avión (pendiente de implementación)\n";
+                break;
+
+            case 6:
+                cout << "Consultar el valor total de ventas por sillas ocupadas (pendiente de implementación)\n";
+                break;
+
+            case 7:
+                cout << "Consultar el promedio de venta por pasajero (pendiente de implementación)\n";
+                break;
+
+            case 8:
+                cout << "Saliendo del programa...\n";
+                break;
+
+            default:
+                cout << "Opción no válida. Intente nuevamente.\n";
+        }
+    } while (opcion != 8);
+
+    return 0;
 }
 
